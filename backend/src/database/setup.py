20d814d49191078c95ff72ffc91fe37076d09d2f
Ivan Kirov <evankirk@vk.com>
2026-02-19 23:02:33 +0500

    Set DB connection stuff

---
+++ backend/src/database/setup.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+from .config import db_config
+    
+
+class Base(DeclarativeBase): 
+    pass
+
+
+async_engine = create_async_engine(  
+            url=db_config.host
+        )
+        
+async_session_maker: type[AsyncSession] = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session():
+    async with async_session_maker() as session:
+        yield session
+
+        
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
