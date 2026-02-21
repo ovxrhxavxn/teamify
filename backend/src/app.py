833dbc6823182392e36c5623653bc0235effbac4
Ivan Kirov <evankirk@vk.com>
2026-02-21 22:37:08 +0500

    Add the lifespan func

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,28 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from .users.router import router as users_router
+from .database.setup import create_tables
 
-app = FastAPI(title="Teamify API")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    yield
+
+
+app = FastAPI(
+
+    title="Teamify API", 
+    lifespan=lifespan
+
+    )
+
+routers = [users_router]
+
+for router in routers:
+    app.include_router(router)
 
 
 app.get("/")
