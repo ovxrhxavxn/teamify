fe2060d60da17a8dae464d69a9e4cac7f45dddf7
Ivan Kirov <evankirk@vk.com>
2026-02-21 22:38:33 +0500

    Distribute types across modules by their purposes

--- backend/src/database/types.py
+++
@@ -1,7 +0,0 @@
-from typing import TypeAlias, TypeVar
-
-
-Entity = TypeVar('Entity')
-EntityId: TypeAlias = int
-
-Model = TypeVar('Model')
---
+++ backend/src/database/types/type_vars.py
@@ -0,0 +1,5 @@
+from typing import TypeVar
+
+
+Entity = TypeVar('Entity')
+Model = TypeVar('Model')
