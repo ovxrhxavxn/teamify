7cf64371d0bf519f6ab0f56a08faa9389ebfbc7b
Ivan Kirov <evankirk@vk.com>
2026-02-23 22:33:27 +0500

    Fix attribute name

--- backend/src/auth/config.py
+++ backend/src/auth/config.py
@@ -3,8 +3,8 @@ from ..config import BaseConfig
 
 class JWTConfig(BaseConfig):
     JWT_SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRES: int = 60*24
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRES: int
 
 
 jwt_config = JWTConfig()
