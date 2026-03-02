f1b6cb491e1cbe1ae960e1db35f57ef76c5598ef
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:28:13 +0500

    Expand FaceitConfig

--- backend/src/faceit/config.py
+++ backend/src/faceit/config.py
@@ -6,6 +6,10 @@ class FaceitConfig(BaseConfig):
     FACEIT_CLIENT_SECRET: str
     FACEIT_AUTH_ENDPOINT: str
     FACEIT_TOKEN_ENDPOINT: str
+    FACEIT_USERINFO_ENDPOINT: str
+    FACEIT_SERVER_API_KEY: str
+    FACEIT_DATA_API_ENDPOINT: str
+    FACEIT_CALLBACK_URI: str
 
 
 faceit_config = FaceitConfig()
