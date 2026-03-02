5e264891454e4d790bb14bfd9c69b567675d7839
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:35:55 +0500

    Add two methods

--- backend/src/reviews/repositories.py
+++ backend/src/reviews/repositories.py
@@ -1,10 +1,12 @@
-from typing import Protocol
+from typing import Protocol, List
 
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, func
 
 from ..database.types.aliases import EntityId
 from ..database.setup import async_session_maker
 from .models import Review
+from ..faceit.models import FaceitData
+from ..users.models import User
 
 
 class AbstractReviewsRepository[Entity](Protocol):
@@ -12,7 +14,10 @@ class AbstractReviewsRepository[Entity](Protocol):
     async def add(self, schema: dict) -> EntityId:
         ...
 
-    async def get_by_profile_id(self, profile_id: int, offset: int, limit: int) -> list[Entity]:
+    async def get_by_profile_id_with_author(self, profile_id: int, offset: int, limit: int) -> List[tuple]:
+        ...
+
+    async def get_average_rating_by_profile_id(self, profile_id: int) -> float:
         ...
     
 
@@ -29,11 +34,32 @@ class ReviewsRepository:
             return result.scalar_one()
         
 
-    async def get_by_user_id(self, profile_id: int, offset: int, limit: int):
+    
+    async def get_by_profile_id_with_author(self, profile_id: int, offset: int, limit: int):
         async with async_session_maker() as session:
-            query = select(self.model).where(self.model.profile_id == profile_id).offset(offset).limit(limit)
-
+            query = (
+                select(
+                    self.model,
+                    FaceitData.nickname,
+                    FaceitData.avatar
+                )
+                .join(User, self.model.author_id == User.id)
+                .join(FaceitData, User.id == FaceitData.user_id)
+                .where(self.model.profile_id == profile_id)
+                .offset(offset)
+                .limit(limit)
+                .order_by(self.model.created_at.desc())
+            )
             result = await session.execute(query)
-            await session.commit()
+            return result.all()
+        
 
-            return result.scalars().all()
+    async def get_average_rating_by_profile_id(self, profile_id: int) -> float:
+        async with async_session_maker() as session:
+            # Создаем запрос, который считает среднее значение (avg) поля rating
+            query = select(func.avg(self.model.rating)).where(self.model.profile_id == profile_id)
+            result = await session.execute(query)
+            # scalar_one_or_none вернет среднее значение или None, если отзывов нет
+            average_rating = result.scalar_one_or_none()
+            # Если отзывов нет, возвращаем 0.0, иначе - посчитанное значение
+            return float(average_rating) if average_rating is not None else 0.0
