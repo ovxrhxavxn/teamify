81f202eaea3c69e239afdceadd366e9ae6da0d85
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:36:23 +0500

    Pass additional repos

--- backend/src/reviews/dependencies.py
+++ backend/src/reviews/dependencies.py
@@ -1,6 +1,9 @@
 from .services import ReviewsService
 from .repositories import ReviewsRepository
+from ..users.repositories import UserRepository
+from ..profiles.repositories import ProfileRepository
+
 
 
 def get_reviews_service() -> ReviewsService:
-    return ReviewsService(ReviewsRepository)
+    return ReviewsService(ReviewsRepository, UserRepository, ProfileRepository)
