af8be6490eccea1c78e280d1d6289a4ef40b94e4
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:44:26 +0500

    Fix attributes

--- backend/src/lfg_statuses/models.py
+++ backend/src/lfg_statuses/models.py
@@ -2,13 +2,12 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 
 from ..database.setup import Base
-from .database.types.annotated_types import intpk
+from ..database.types.annotated_types import intpk
 
 
 class LFGStatus(Base):
-
     __tablename__ = "lfg_statuses"
 
     id: Mapped[intpk]
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    is_active: Mapped[bool] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+    is_active: Mapped[bool] = mapped_column(default=False)
