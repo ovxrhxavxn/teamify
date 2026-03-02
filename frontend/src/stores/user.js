a7c27d5f44477d71a22302cc254ec76180eac028
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:26:57 +0500

    Add user store

---
+++ frontend/src/stores/user.js
@@ -0,0 +1,70 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import router from '@/router'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    profile: null,
+    isLoading: false,
+    isSaving: false,
+  }),
+  getters: {
+    isAuthenticated: (state) => !!state.profile,
+  },
+  actions: {
+    async fetchUser() {
+      const token = localStorage.getItem('user_token')
+      if (!token) {
+        return
+      }
+      if (this.profile) {
+        return
+      }
+      this.isLoading = true
+      try {
+        const response = await axios.get('https://teamify.pro/api/profiles/me', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        this.profile = response.data
+      } catch (error) {
+        console.error('Ошибка аутентификации, токен невалиден:', error)
+        this.logout()
+      } finally {
+        this.isLoading = false
+      }
+    },
+    async updateProfileDescription(newDescription) {
+      this.isSaving = true
+      try {
+        const token = localStorage.getItem('user_token')
+        await axios.put(
+          'https://teamify.pro/api/profiles/me',
+          { description: newDescription },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        )
+        // При успехе обновляем состояние локально, без перезагрузки
+        if (this.profile) {
+          this.profile.profile.description = newDescription
+        }
+      } catch (error) {
+        console.error('Ошибка при обновлении профиля:', error)
+        // Здесь можно показать уведомление об ошибке
+      } finally {
+        this.isSaving = false
+      }
+    },
+    logout() {
+      this.profile = null // Очищаем данные
+      localStorage.removeItem('user_token')
+      if (router.currentRoute.value.name !== 'home') {
+        router.push({ name: 'home' })
+      }
+    },
+  },
+})
