013395d78847a5a6fe70b5c96169f6ad7cb44044
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:45:30 +0500

    Degine server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // или '0.0.0.0'
+    // Вы также можете указать порт, если нужно
+    // port: 5173
+
+    allowedHosts: ['teamify.pro'],
+  },
   plugins: [vue(), vueDevTools(), vueJsx(), tailwindcss()],
   resolve: {
     alias: {
