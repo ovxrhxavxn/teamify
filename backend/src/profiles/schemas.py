7fc751122e757cb7f0c284613d0d38fd1749ea11
Ivan Kirov <evankirk@vk.com>
2026-03-02 23:37:11 +0500

    Add user profile respinse and update schemas

--- backend/src/profiles/schemas.py
+++ backend/src/profiles/schemas.py
@@ -1,13 +1,26 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
+from ..faceit.schemas import FaceitDataFromDB
 
 
 class Profile(BaseModel):
     user_id: int
-    description: str
+    description: str | None = None
 
 
 class ProfileFromDB(Profile):
     id: int
-
     class Config:
         from_attributes = True
+
+
+class UserProfileResponse(BaseModel):
+    profile: ProfileFromDB
+    faceit_data: FaceitDataFromDB
+    rating: float
+
+
+class ProfileUpdate(BaseModel):
+    description: Optional[str] = None
