ca3e1452ef55fc828879204fd3e75e35f6958802
Ivan Kirov <evankirk@vk.com>
2026-03-03 11:47:37 +0500

    Implement counting of total profile reviews

--- backend/src/profiles/schemas.py
+++ backend/src/profiles/schemas.py
@@ -20,7 +20,7 @@ class UserProfileResponse(BaseModel):
     profile: ProfileFromDB
     faceit_data: FaceitDataFromDB
     rating: float
-
+    total_reviews: Optional[int] = None
 
 class ProfileUpdate(BaseModel):
     description: Optional[str] = None
