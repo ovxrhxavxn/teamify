5ee2af7cd73ca7745ee8847fb6fe089fced6d90f
Ivan Kirov <evankirk@vk.com>
2026-03-04 02:04:26 +0500

    Add ant design config

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,11 +4,19 @@ import { useUserStore } from '@/stores/user'
 
 const userStore = useUserStore()
 
+const theme = {
+  token: {
+    colorPrimary: '#000000',
+  },
+}
+
 onMounted(() => {
   userStore.fetchUser()
 })
 </script>
 
 <template>
-  <router-view />
+  <a-config-provider :theme="theme">
+    <router-view />
+  </a-config-provider>
 </template>
