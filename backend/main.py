8e9486e3596268b3f843f1e8eeb750b6a056ff96
Ivan Kirov <evankirk@vk.com>
2026-03-04 00:07:13 +0500

    main

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,8 @@ def main():
     uvicorn.run(
 
         app="src.app:app",
-        host="192.168.3.37"
+        host="192.168.3.37",
+        reload=True
     )
 if __name__ == "__main__":
     main()
